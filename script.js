b2dfaf3320a8a2bc71449205943d8df534bbad98
Pvilchisrizo <palomichys@gmail.com>
2026-05-16 19:44:06 -0700

    update baseURL

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const form = document.getElementById("new-post-form");
-const baseURL = `http://localhost:3000`;
-//const baseURL = `https://megaphone-server.onrender.com`;
+const baseURL = `https://megaphone-backend-paloma.onrender.com`;
 
 const getPosts = async () => {
   const response = await fetch(`${baseURL}/posts`);
